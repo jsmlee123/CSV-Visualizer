8e7df65fb2c372d5bb3ff6146175ec66930568e9
jsmlee <jsmlee123@yahoo.com>
2021-09-18 21:15:01 -0400

    Testing Save
    Tested Saving altered images

---
+++ hackmit-p1/src/Commands/LinearRegressionCommand.java
@@ -0,0 +1,17 @@
+package Commands;
+
+import Model.Country;
+import java.util.List;
+import view.DataView;
+
+public class LinearRegressionCommand extends AbstractCommand{
+
+  public LinearRegressionCommand(List<Country> model, DataView view) {
+    super(model, view);
+  }
+
+  @Override
+  public void run() {
+
+  }
+}
